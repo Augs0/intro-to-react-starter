7b2abc66b418c1953b8ecdbe2d42b0fb0aa1838e
August Aldred <augustaldred@bpp.com>
2020-07-21 16:00:27 +0100

    first commit

---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <>
+            <div className="wrapper">
+                <p>Name: </p>
+                <p>Languages: </p>
+                <p>Tabs or spaces?</p>
+            </div>
+        </>
+    )
+}
